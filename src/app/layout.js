316c2f22eeda14db9c9b9af097c35950de173c8a
Iyan Theekshana <iyan.dev123@gmail.com>
2025-11-24 19:42:40 +0100

    added search console meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ export const metadata = {
   title: "Scuola Finder",
   description:
     "Scuola Finder ti aiuta a esplorare tipi di istruzione, settori e scuole a Milano",
+  verification: {
+    google: "iv4xz7NfdtVpI6PthR0aOD-_ceDY2D4VwVTzG04Zd40",
+  },
 };
 
 export default function RootLayout({ children }) {
